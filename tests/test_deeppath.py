173108a5497316e23bc7d0ffb97c4cc05799d81d
Quentin BERNARD <Quentin.BERNARD@amadeus.com>
2020-10-21 17:31:06 +0100

    Adding failing tests for dget (skipped)

--- tests/test_deeppath.py
+++ tests/test_deeppath.py
@@ -25,15 +25,121 @@ def test_dget_basic():
 def test_dget_repetitions():
     """Check that repetitions are correctly handled"""
     data = {"deeply": {"nested": [{"path": 2}, {"path": 3}, {"path": 4}]}}
-    assert dget(data, "deeply/nested[0]/path") == 2
-    assert not dget(data, "deeply/nested[10]/path")
-    assert dget(data, "deeply/nested[-1]/path") == 4
+    assert dget(data, f"deeply/nested[0]/path") == 2
+    assert not dget(data, f"deeply/nested[10]/path")
+    assert dget(data, f"deeply/nested[-1]/path") == 4
 
 
+@pytest.mark.skip
+def test_dget_repetition_from_start():
+    data = [1, 2]
+    assert dget(data, "[0]") == 1
+    assert dget(data, "[1]") == 2
+
+
+@pytest.mark.skip
+def test_dget_flatten_list():
+    """
+    Check flattening a list
+    """
+    data = [
+        {"a": [1, 2]},
+        {"b": [3, 4]}
+    ]
+    assert dget(data, "[*]") == data
+    assert dget(data, "[*]/a") == [[1, 2]]
+    assert dget(data, "[*]/a[*]") == [1, 2]
+    assert dget(data, "[1]/b[*]") == [3, 4]
+
+
+@pytest.mark.skip
+def test_dget_flatten_incompatible_list_dict():
+    """
+    What happens if you flatten a dict with list syntax, or a list with
+    dict syntax ?
+    """
+    data = {
+        "list": [1, 2],
+        "dict": {"1": 1, "2": 2}
+    }
+    assert dget(data, "list/*") is None
+    assert dget(data, "dict[*]") is None
+
+
+@pytest.mark.skip
+def test_dget_double_flatten():
+    data = {
+        "a": {
+            "b": {
+                "c": 1
+            },
+            "b2": {
+                "c": 2
+            }
+        }
+    }
+    assert dget(data, "*") == [{'b': {'c': 1}, 'b2': {'c': 2}}]
+    assert dget(data, "*/*") == [{'c': 1}, {'c': 2}]
+    assert dget(data, "*/*/c") == [1, 2]
+
+
+@pytest.mark.skip
+def test_dget_flatten_excludes_unmatched_path():
+    data = {
+        "a": {
+            "b": {
+                "c": 1
+            },
+            "b2": {
+                "c": 2
+            }
+        }
+    }
+    assert dget(data, "*/b2/*") == [2]
+
+
+def test_dget_flatten_form_start():
+    """
+    Check the flattening works from the start of the structure
+    """
+    data = { "any1": 1, "any2": 2 }
+    assert dget(data, "*") == [1, 2]
+
+
+@pytest.mark.skip
+def test_dget_flatten_and_repetition():
+    """
+    Check that the flatten and repetitions features are compatible
+    """
+    reps = [
+        {
+            "nested_in_rep": 1,
+        },
+        {
+            "nested_in_rep": 2,
+            "other_nested": {"other": 3}
+        }
+    ]
+    data1 = {
+        "flattened": reps
+    }
+    data2 = {
+        "a": [1, 2],
+        "b": [3, 4]
+    }
+    assert dget(data1, "*") == [reps]
+    assert dget(data1, "flattened[0]") == {"nested_in_rep": 1}
+    assert dget(data1, "*/nested_in_rep") is None  # */ is a list, it needs explicit unfold
+    assert dget(data1, "*[*]/nested_in_rep") == [1, 2]
+    assert dget(data2, "*[0]") == [1, 3]
+    assert dget(data2, "*/a") is None
+
+
+@pytest.mark.skip
 def test_dget_flatten():
     """Check that we can successfully flatten a nested structure"""
     data = {"deeply": {"nested": [{"path": 2}, {"path": 3}, {"path": 4}]}}
-    assert dget(data, "deeply/*/path") == [[2, 3, 4]]
+    assert dget(data, "deeply/*[*]/path") == [2, 3, 4]
     data = {
         "deeply": {"nested": {"path": 2}, "other": {"path": 3}, "more": {"path": 4}}
     }
