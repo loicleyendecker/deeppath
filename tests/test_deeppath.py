b9b8b3901526c4c1c1c756708c0fda45be2bfa40
1Mark <15360245+1Mark@users.noreply.github.com>
2021-11-30 10:25:56 +0000

    Add flatten

--- tests/test_deeppath.py
+++ tests/test_deeppath.py
@@ -1,7 +1,4 @@
-"""
-Validate the dget function that accesses values in nested dictionaries,
-using the xpath syntax
-"""
+"""Validate the dget function that accesses values in nested dictionaries, using the xpath syntax"""
 
 import datetime
 from dataclasses import dataclass
@@ -9,7 +6,7 @@ from typing import List
 
 import pytest
 
-from deeppath import dget, dset, dwalk
+from deeppath import dget, dset, dwalk, flatten
 
 
 def test_dget_basic():
@@ -142,16 +139,10 @@ def test_dget_flatten_and_repetition():
 @pytest.mark.skip
 def test_dget_flatten():
     """Check that we can successfully flatten a nested structure"""
+    data = {"deeply": {"nested": [{"path": 2}, {"path": 3}, {"path": 4}]}}
+    assert dget(data, "deeply/*/path") == [[2, 3, 4]]
     data = {
-        "deeply": {
-            "nested": [{"path": 2}, {"path": 3}, {"path": 4}]
-        }
-    }
-    assert dget(data, "deeply/*[*]/path") == [2, 3, 4]
-    data = {
-        "deeply": {
-            "nested": {"path": 2}, "other": {"path": 3}, "more": {"path": 4}
-        }
+        "deeply": {"nested": {"path": 2}, "other": {"path": 3}, "more": {"path": 4}}
     }
     assert dget(data, "deeply/*/path") == [2, 3, 4]
 
@@ -179,12 +170,9 @@ def test_decoded_classes():
     data = {
         "deeply": {"nested": {"name": "John"}},
         "somewhere": {"else": {"age": 25}},
-        "other": {
-            "location": {"birthday": {"year": 2020, "month": 1, "day": 20}}
-        },
+        "other": {"location": {"birthday": {"year": 2020, "month": 1, "day": 20}}},
     }
-    assert Person("John", 25, datetime.date(2020, 1, 20)) ==\
-           Person.from_dict(data)
+    assert Person("John", 25, datetime.date(2020, 1, 20)) == Person.from_dict(data)
 
 
 def test_extended_class():
@@ -205,9 +193,7 @@ def test_extended_class():
     data = {
         "deeply": {"nested": {"name": "John"}},
         "somewhere": {"else": {"age": 25}},
-        "other": {
-            "location": {"birthday": {"year": 2020, "month": 1, "day": 20}}
-        },
+        "other": {"location": {"birthday": {"year": 2020, "month": 1, "day": 20}}},
         "list": {
             "of": {
                 "hobbies": [
@@ -229,24 +215,24 @@ def test_dset():
     assert data == {"some": {"new": {"value": 1}}}, "Simple dset OK"
 
     dset(data, "repetition[0]", 2)
-    assert data == {"some": {"new": {"value": 1}}, "repetition": [2]},\
-        "Simple dset with repetition OK"
+    assert data == {
+        "some": {"new": {"value": 1}},
+        "repetition": [2],
+    }, "Simple dset with repetition OK"
 
     data = {}
     dset(data, "nested[0]/repetition/value", 1)
-    assert data == {"nested": [{"repetition": {"value": 1}}]},\
-        "Repetition with nested value OK"
+    assert data == {
+        "nested": [{"repetition": {"value": 1}}]
+    }, "Repetition with nested value OK"
 
     data = {}
     dset(data, "multiple[0]/repetition[0]", 1)
-    assert data == {"multiple": [{"repetition": [1]}]},\
-        "Multiple repetitions OK"
+    assert data == {"multiple": [{"repetition": [1]}]}, "Multiple repetitions OK"
     dset(data, "multiple[1]", 2)
-    assert data == {"multiple": [{"repetition": [1]}, 2]},\
-        "Appending to repetitions OK"
+    assert data == {"multiple": [{"repetition": [1]}, 2]}, "Appending to repetitions OK"
     dset(data, "/multiple[2]", 3)
-    assert data == {"multiple": [{"repetition": [1]}, 2, 3]},\
-        "Leading '/' supported"
+    assert data == {"multiple": [{"repetition": [1]}, 2, 3]}, "Leading '/' supported"
 
     with pytest.raises(IndexError):
         dset(data, "multiple[5]", 1)
@@ -258,17 +244,22 @@ def test_dwalk():
     """
     data = {
         "value": 1,
-        "nested": {
-            "other": 2
-        },
-        "repetition": [
-            "repetition1",
-            {"inside": "repetition"}
-        ]
+        "nested": {"other": 2},
+        "repetition": ["repetition1", {"inside": "repetition"}],
     }
     assert list(dwalk(data)) == [
         ("value", 1),
         ("nested/other", 2),
         ("repetition[0]", "repetition1"),
-        ("repetition[1]/inside", "repetition")
+        ("repetition[1]/inside", "repetition"),
+    ]
+
+
+def test_flatten():
+    """Ensure nested structures are flattened"""
+    assert flatten([[[[[1]]]], [2]]) == [1, 2]
+    assert flatten([[[[[[[[1]]]]]]]]) == [1]
+    assert flatten([[]]) == []
+    assert flatten([[{"documentDetails": {"number": "0"}}]]) == [
+        {"documentDetails": {"number": "0"}}
     ]
