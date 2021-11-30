5331bd250ea7a393bacefa4e4d7ac5802bab8505
1Mark <15360245+1Mark@users.noreply.github.com>
2021-11-30 14:47:00 +0000

    Reformat files using isort and black

--- deeppath/__init__.py
+++ deeppath/__init__.py
@@ -1,7 +1,7 @@
 """Top-level package for deeppath."""
 
 __author__ = """Loic Leyendecker"""
-__email__ = 'loic.leyendecker@gmail.com'
-__version__ = '0.1.1'
+__email__ = "loic.leyendecker@gmail.com"
+__version__ = "0.1.1"
 
-from deeppath.deeppath import dget, dset, dwalk
+from deeppath.deeppath import dget, dset, dwalk, flatten
--- setup.py
+++ setup.py
@@ -2,46 +2,48 @@
 
 """The setup script."""
 
-from setuptools import setup, find_packages
+from setuptools import find_packages, setup
 
-with open('README.rst') as readme_file:
+with open("README.rst") as readme_file:
     readme = readme_file.read()
 
-with open('HISTORY.rst') as history_file:
+with open("HISTORY.rst") as history_file:
     history = history_file.read()
 
-requirements = [ ]
+requirements = []
 
-setup_requirements = ['pytest-runner', ]
+setup_requirements = [
+    "pytest-runner",
+]
 
-test_requirements = ['pytest>=3', 'dataclasses; python_version < "3.7"']
+test_requirements = ["pytest>=3", 'dataclasses; python_version < "3.7"']
 
 setup(
     author="Loic Leyendecker",
-    author_email='loic.leyendecker@gmail.com',
-    python_requires='>=3.6',
+    author_email="loic.leyendecker@gmail.com",
+    python_requires=">=3.6",
     classifiers=[
-        'Development Status :: 2 - Pre-Alpha',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: MIT License',
-        'Natural Language :: English',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
+        "Development Status :: 2 - Pre-Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
     ],
     description="Python module to easily manipulate complex nested structures",
     install_requires=requirements,
     license="MIT license",
-    long_description=readme + '\n\n' + history,
+    long_description=readme + "\n\n" + history,
     include_package_data=True,
-    keywords='deeppath',
-    name='deeppath',
-    packages=find_packages(include=['deeppath', 'deeppath.*']),
+    keywords="deeppath",
+    name="deeppath",
+    packages=find_packages(include=["deeppath", "deeppath.*"]),
     setup_requires=setup_requirements,
-    test_suite='tests',
+    test_suite="tests",
     tests_require=test_requirements,
-    url='https://github.com/loicleyendecker/deeppath',
-    version='0.1.1',
+    url="https://github.com/loicleyendecker/deeppath",
+    version="0.1.1",
     zip_safe=False,
 )
