4efa0f30d70a2f18f1c4e7b796a16554e40cbdf1
Mark <15360245+1Mark@users.noreply.github.com>
2022-03-10 20:36:16 +0000

    Improve package versioning to resolve #7
    - Add automatic version bumping when a new release is created via
    https://github.com/loicleyendecker/deeppath/releases/new
    - tag name must be semver and follow va.b.c
    - move bump2version config from setup.cfg to .bumpversion.cfg
    since it will delete any comments in the cfg files
    see https://github.com/c4urself/bump2version/issues/37

--- deeppath/__init__.py
+++ deeppath/__init__.py
@@ -1,7 +1,7 @@
-"""Top-level package for deeppath."""
+"""Easily access nested structures with using an xpath like syntax."""
 
-__author__ = """Loic Leyendecker"""
+__author__ = "Loic Leyendecker"
 __email__ = "loic.leyendecker@gmail.com"
-__version__ = "0.1.1"
+__version__ = "0.1.4"
 
 from deeppath.deeppath import dget, dset, dwalk, flatten
