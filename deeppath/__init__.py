c1422bb3731336f36b9d5315158365082491f620
Loïc Leyendecker <loic.leyendecker@gmail.com>
2024-03-25 07:48:51 +0000

    Modernize and lint

--- deeppath/__init__.py
+++ deeppath/__init__.py
@@ -3,8 +3,8 @@
 __author__ = """Loic Leyendecker"""
 __email__ = "loic.leyendecker@gmail.com"
 
-from ._version import version as __version__, version_tuple
-
+from ._version import version as __version__
+from ._version import version_tuple
 from .deeppath import dget, dset, dwalk, flatten
 
 __all__ = ["dget", "dset", "dwalk", "flatten", "__version__", "version_tuple"]
